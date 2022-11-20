ced12730f5a90b47321f90d4407e4b50939478f3
Sinaf19 <quentinsurdez@gmail.com>
2022-11-20 06:48:39 +0100

    guessing game not finished

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 
 
+
+use std::io;
+use rand::Rng;
+
 fn main() {
 
-    // ici on peut voir un commentaire
     println!("Hello, world!");
+
+    guessing_game();
+}
+
+fn guessing_game() {
+
+    println!("Guess a number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {guess}");
+
 }
